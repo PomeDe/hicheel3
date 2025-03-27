fe6aa7771735c6ddc9cc67911acf551f69e01f2b
Rana <93430019+PomeDe@users.noreply.github.com>
2025-03-27 14:05:32 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ const data = [
   },
   {
     id:3,
-    fname:"anar",
+    fname:"anduul",
     lname:"batsukh",
     job: "Unemployed",
     image: "https://i.pinimg.com/originals/41/f7/dd/41f7dd66257ec0914e9267e4dcbc76ed.jpg",
@@ -89,7 +89,7 @@ export default function Home() {
   const [togle, setTogle] = useState(true);
   const searchFilter =(array) =>{
     return array.filter(
-      (e) => (e.fname + e.lname).toLowerCase().includes(val.toLocaleLowerCase())
+      (el) => (el.fname+el.lname).toLowerCase().includes(val.toLocaleLowerCase())
     )
   }
   const filtered = searchFilter(data)
@@ -98,7 +98,7 @@ export default function Home() {
   };
 
   return (
-<div className="p-10 bg-slate-50 h-screen">
+<div className="p-10 bg-white w-screen h-screen">
       <div className="flex flex-row w-full mx-auto mb-5 justify-between">
         <input
             type="text"
@@ -109,12 +109,12 @@ export default function Home() {
           />
           <button className="" onClick={() => setTogle(!togle)}>Toggle</button>
       </div>
-      {togle ? (<div className="grid gap-10">
+      {togle ? (<div className="grid grid-cols-4 gap-24">
       {filtered.length >0 ? (
       filtered.map((element) => (
           <div
             key={element.id}
-            className="w-84 h-84 flex flex-col p-5 rounded-lg text-black shadow-xl border-black bg-slate-200"
+            className="w-84 h-84 flex flex-col p-5 rounded-lg text-black shadow-xl border-black bg-slate-200 transition delay-100 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110"
           >
             <div
             className="w-full h-1/2">
@@ -139,7 +139,38 @@ export default function Home() {
             </div>
           </div>
         ))):(<p className="text-red-500 text-center text-xl col-span-full">Can't find the user</p>)}
-      </div>): (<p>Not grid</p>)}
+      </div>): (<div className="grid grid-cols-2  w-full">
+      {filtered.length >0 ? (
+      filtered.map((element) => (
+          <div
+            key={element.id}
+            className="w-full h-60 flex flex-row p-5 text-black  shadow-md border-black border-2 bg-slate-200 "
+          >
+            <div
+            className="w-1/2 h-full">
+<img
+              src={element.image}
+              className=" w-full h-full" 
+            />
+            </div>
+            <div className="flex flex-col">
+            <div className="text-black mb-2 ml-2">
+              <p className="text-black">Firstname: {element.fname}</p>
+            </div>
+            <p className="text-black ml-2">Lastname: {element.lname}</p>
+            </div>
+            <p className="text-blue-700 ml-3">Job: {element.job}</p>
+            <div className=" ml-5">
+              <p className="text-blue-400 font-bold">Items:</p>
+              <ul className="list-disc ml-5 text-black">
+                {element.items.map((item) => (
+                  <li key={item.id}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))):(<p className="text-red-500 text-center text-xl col-span-full">Can't find the user</p>)}
+      </div>)}
       </div>
       
   );
